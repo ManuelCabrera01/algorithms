a18090679199bf3685a3e88b8e5de47ddfa534f5
Manuel <m.a.c.p90@gmail.com>
2018-09-10 15:11:44 -0400

    my solution

---
+++ Big-o.js
@@ -0,0 +1,35 @@
+// constant runtime
+//Big o notation : '0(1)'
+
+// is when the imput size increase the number of operatione that we perfom never changes we still log out the first two element of the array no matter how mutch increase the array
+function log(array) {
+  console.log(array[0]);
+  console.log(array[1]);
+}
+
+log([1, 2, 3, 4, 5, 6]);
+
+//linear runtime
+//Big o notation  : "o(n)"
+// we have to do a aporation in evre single element in the array so if the size of the input increase also the runtime and for this function the runtime increase proportionally as the input increase
+function logAll(array) {
+  for (var i = 0; i <= array.length; i++) {
+    console.log(array[i]);
+  }
+}
+logAll([1, 2, 3, 4, 5]);
+logAll([1, 2, 3, 4, 5, 7, 8, 9, 0]);
+
+// Exponential runtime
+// Big O notation :"o (n^2)"
+// it is not efficiente
+function addAndLog(array) {
+  for (var i = 0; i <= array.length; i++) {
+    for (var j = 0; j <= array.length; j++) {
+      console.log(array[i] + array[j]);
+    }
+  }
+}
+addAndLog(["a", "b", "c"]); // 9 pairs logged out
+addAndLog(["a", "b", "c", "d"]); // 16 pairs logged out
+addAndLog(["a", "b", "c", "d", "e"]); // 25 pairs logged out
---
+++ HarmlessRansomNote.js
@@ -0,0 +1,24 @@
+// to solve this is important to understand BigO notation and time complexity
+
+function harmlessRansomNote(noteText, magazineText) {
+  var newnoteText = noteText.split(" ");
+  var newmagazineText = magazineText.split(" ");
+  var compare = [];
+  console.log(compare);
+
+  for (var i = 0; i <= newnoteText.length; i++) {
+    for (var j = 0; j < newmagazineText.length; j++) {
+      if (newnoteText[i] === newmagazineText[j]) {
+        compare.push(newmagazineText[j]);
+      }
+    }
+  }
+  if (compare.length === newnoteText.length) {
+    console.log(true);
+  } else console.log(false);
+  console.log(compare);
+}
+
+var note = "this is the note";
+var magazing = "this is the note ";
+harmlessRansomNote(note, magazing);
