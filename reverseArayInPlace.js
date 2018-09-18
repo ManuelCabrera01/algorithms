9f2585374f7160a5971b4ab3f840aeb0b14f3358
Manuel <m.a.c.p90@gmail.com>
2018-09-18 09:04:45 -0400

    bad solution and good solution

--- reverseArayInPlace.js
+++ reverseArayInPlace.js
@@ -1,17 +1,27 @@
+// not working solution
+
 const reverArrayInPlace = stri => {
   let arr = stri.split(" ");
   let myObj = {};
-  //   for (var i = 0; i <= arr.legth - 1; i++) {
 
-  //   }
   for (var j = arr.length; j >= 0; j--) {
     console.log();
-    myObj[arr[j]] = 0;
-    myObj[arr[j]]++;
+    myObj[arr[j]] = myObj;
   }
-  console.log(myObj);
 };
 
 reverArrayInPlace(
   "Lorem ipsum dolor sit amet consectetur adipisicing elit Laborum similique"
 );
+
+function reverseArrayInPlace(arr) {
+  for (var i = 0; i < arr.length / 2; i++) {
+    var tempVar = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tempVar;
+  }
+
+  return arr;
+}
+
+reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8]);
